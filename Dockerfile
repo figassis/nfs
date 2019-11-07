7aaced9d00c41b84a60f0d5cb413ce1220fa09f1
Assis Ngolo <figassis@gmail.com>
2019-11-07 23:55:48 +0100

    unified storage backends

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,28 @@
 FROM ubuntu:bionic
 
+# local, goofys, objectivefs
+ENV STORAGE="local"
+
 ENV AWS_ACCESS_KEY_ID=""
 ENV AWS_SECRET_ACCESS_KEY=""
 ENV AWS_DEFAULT_REGION="us-east-1"
-ENV AWS_S3_BUCKET=""
-
 ENV BUCKET=""
 
+#GOOFYS
+ENV GOOFYS_DIR_MODE="0777"
+ENV GOOFYS_FILE_MODE="0755"
+ENV GOOFYS_UID=""
+ENV GOOFYS_GID=""
+ENV GOOFYS_ENDPOINT="s3.amazonaws.com"
+ENV GOOFYS_REGION=${AWS_DEFAULT_REGION}
+ENV GOOFYS_STORAGE_CLASS="STANDARD"
+ENV GOOFYS_USE_CONTENT_TYPE=""
+ENV GOOFYS_STAT_CACHE_TTL="1m0s"
+ENV GOOFYS_TYPE_CACHE_TTL="1m0s"
+ENV GOOFYS_GLOBAL_OPTION='-o nosuid,nodev,rw,allow_other,nonempty --cache "--free:10%:/var/cache/data"'
+ENV GOOFYS_ETC_MIME=""
+
+# OBJECTIVEFS
 ENV OBJECTIVEFS_LICENSE=""
 
 #The filesystem passphrase is required to mount your filesystem
@@ -20,25 +36,30 @@ ENV CACHESIZE="20%"
 #<DISK CACHE SIZE>[:<FREE SPACE>]
 ENV DISKCACHE_SIZE="2G:100G"
 
-# install prerequisites
-ADD https://s3.amazonaws.com/files.nellcorp.com/assets/ofs/objectivefs_6.4_amd64.deb /
+COPY bin/goofys-v0.22.0 /usr/local/bin/goofys
+COPY bin/catfs-v0.8.0 /usr/local/bin/catfs
+ADD bin/objectivefs_6.4_amd64.deb /
+
+ADD docker-entrypoint.sh /
+ADD conf/local.conf /etc/
+ADD conf/goofys.conf /etc/
+ADD conf/objectivefs.conf /etc/
+
 RUN apt-get update \
     && apt-get install -y libfuse2 fuse rsyslog nfs-ganesha nfs-ganesha-vfs nfs-ganesha-proxy vim git procps unzip supervisor \
     && dpkg -i objectivefs_6.4_amd64.deb \
     && rm objectivefs_6.4_amd64.deb \
     && apt-get clean \
     && rm -rf /var/lib/apt/lists/* \
-    && mkdir /var/run/dbus
+    && mkdir -p /var/run/dbus /var/run/dbus /export /var/cache/data /var/log/supervisor \
+    && chmod +x /docker-entrypoint.sh /usr/local/bin/goofys /usr/local/bin/catfs \
+    && rm /etc/mtab && ln -s /proc/mounts /etc/mtab
 
+ADD conf/ganesha.conf /etc/ganesha/
 
-ADD docker-entrypoint.sh /
-ADD ganesha.conf /etc/ganesha/
-ADD supervisord.conf /etc/
+VOLUME /export /var/cache/data
 
-RUN chmod +x /docker-entrypoint.sh && mkdir -p /objectivefs /var/log/supervisor
-
-VOLUME /objectivefs
-
-EXPOSE 2049 111/tcp 111/udp 20048
+#EXPOSE 2049 111/tcp 111/udp 20048
+EXPOSE 2049/tcp
 
 CMD ["/docker-entrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,10 +2,64 @@
 #set -e
 #set -x
 
+# SETUP GOOFYS
+###############################################################
+mkdir -p ~/.aws
+
+cat <<- EOF > ~/.aws/credentials
+[default]
+aws_access_key_id = $AWS_ACCESS_KEY_ID
+aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
+EOF
+
+cat <<- EOF > ~/.aws/config
+[profile default]
+region = $AWS_DEFAULT_REGION
+EOF
+
+if [[ ! -z "$GOOFYS_ETC_MIME" ]]; then
+  cat <<- EOF > /etc/mime.types
+$GOOFYS_ETC_MIME
+EOF
+fi
+
+if [[ -z "$GOOFYS_REGION" ]]; then
+  GOOFYS_REGION="$AWS_DEFAULT_REGION"
+fi
+
+# goofys global option setting
+[[ ! -z "$GOOFYS_DIR_MODE" ]] && GOOFYS_GLOBAL_OPTION="${GOOFYS_GLOBAL_OPTION} --dir-mode ${GOOFYS_DIR_MODE}"
+[[ ! -z "$GOOFYS_FILE_MODE" ]] && GOOFYS_GLOBAL_OPTION="${GOOFYS_GLOBAL_OPTION} --file-mode ${GOOFYS_FILE_MODE}"
+[[ ! -z "$GOOFYS_UID" ]] && GOOFYS_GLOBAL_OPTION="${GOOFYS_GLOBAL_OPTION} --uid ${GOOFYS_UID}"
+[[ ! -z "$GOOFYS_GID" ]] && GOOFYS_GLOBAL_OPTION="${GOOFYS_GLOBAL_OPTION} --gid ${GOOFYS_GID}"
+[[ ! -z "$GOOFYS_ENDPOINT" ]] && GOOFYS_GLOBAL_OPTION="${GOOFYS_GLOBAL_OPTION} --endpoint ${GOOFYS_ENDPOINT}"
+[[ ! -z "$GOOFYS_REGION" ]] && GOOFYS_GLOBAL_OPTION="${GOOFYS_GLOBAL_OPTION} --region ${GOOFYS_REGION}"
+[[ ! -z "$GOOFYS_STORAGE_CLASS" ]] && GOOFYS_GLOBAL_OPTION="${GOOFYS_GLOBAL_OPTION} --storage-class ${GOOFYS_STORAGE_CLASS}"
+[[ ! -z "$GOOFYS_USE_CONTENT_TYPE" ]] && GOOFYS_GLOBAL_OPTION="${GOOFYS_GLOBAL_OPTION} --use-content-type ${GOOFYS_USE_CONTENT_TYPE}"
+[[ ! -z "$GOOFYS_STAT_CACHE_TTL" ]] && GOOFYS_GLOBAL_OPTION="${GOOFYS_GLOBAL_OPTION} --stat-cache-ttl ${GOOFYS_STAT_CACHE_TTL}"
+[[ ! -z "$GOOFYS_TYPE_CACHE_TTL" ]] && GOOFYS_GLOBAL_OPTION="${GOOFYS_GLOBAL_OPTION} --type-cache-ttl ${GOOFYS_TYPE_CACHE_TTL}"
+
+# START
+######################################################
 
 rpcbind
 rpc.statd -L
 rpc.idmapd
 dbus-uuidgen --ensure
 dbus-daemon --system --fork
-exec /usr/bin/supervisord -n -c /etc/supervisord.conf
+
+case $STORAGE in
+
+  goofys)
+    exec /usr/bin/supervisord -n -c /etc/goofys.conf  
+    ;;
+
+  objectivefs)
+    exec /usr/bin/supervisord -n -c /etc/objectivefs.conf  
+    ;;
+
+  *)
+    echo -n "Invalid storage option $STORAGE. Starting local NFS server";
+    exec /usr/bin/supervisord -n -c /etc/local.conf
+    ;;
+esac
